07f65b8c96f089695c4a3417f3a0c75c46b66783
Danielle Byrne <DanielleByrne421@gmail.com>
2020-08-07 10:41:11 -0400

    set up initial files

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./container/Home/Home"; 
+
+function App() {
+  useEffect(() => {
+    axios
+      .get("/api/config")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/> 
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
