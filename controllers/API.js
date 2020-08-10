23dc3b0ace73350305e7f3af53fc36e28e142a21
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-08-10 10:23:08 -0500

    Edited routes using Ripping Recipes as example

--- controllers/API.js
+++ controllers/API.js
@@ -4,35 +4,43 @@ const db = require("../models/index")
 
 //Create user (using that cronjon?)
 router.post("/api/signup", function(req,res){
-    db.Users.create(req.body).then(res => {
+    db.User.create(req.body).then(res => {
         res.json(res)
     })
 })
 
 //Get workout calendar or list?
 router.get("/api/workout", function(req, res) {
-    db.Workouts.find({}).then(res =>{
+    db.Workout.findAll({}).then(res =>{
+        res.json(res)
+    })
+})
+
+
+//Get user (for dashboard purposes)
+router.get("/api/user/:id", function(req, res) {
+    db.User.findOne({}).then(res =>{
         res.json(res)
     })
 })
 
 //Create workout (using that cronjon?)
 router.post("/api/user/:id", function(req,res){
-    db.Workouts.create(req.body).then(res => {
+    db.Workout.create(req.body).then(res => {
         res.json(res)
     })
 })
 
 //Update workout (false --> true)
-router.update("/api/workout/:id", function(req,res){
-    db.Workouts.findOneAndUpdate(req.params).then(res => {
+router.put("/api/workout/:id", function(req,res){
+    db.Workout.findOneAndUpdate(req.params).then(res => {
         res.json(res)
     })
 })
 
 //Delete account
 router.delete("/api/user/:id", function(req,res){
-    db.Books.destroy(req.params).then(res => {
+    db.User.destroy(req.params).then(res => {
         res.json(res)
     })
 })
