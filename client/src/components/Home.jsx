11634f4e008d56fe40dff91a1c8b45bfe13b1f98
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-08-16 16:06:57 -0500

    Deleted comments from Home file

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,29 +1,13 @@
-// import React from 'react';
-// // THIS IS A TEST COMPONENT. FEEL FREE TO REMOVE WHEN MORE IN DEPTH STYLING IS DONE
-// const Home = () => {
-//     return (
-//         <div>
-//             <h1>YOU WERE AUTHENTICATED I THINK</h1>
-//         </div>
-//     );
-// };
-
-// export default Home;
-
 import React, { Component } from "react";
-// import fire from "../config/Fire";
+
 import Login from "./login"
 
 class Home extends Component {
   
 
-  // componentDidMount() {
-  //   this.authListener();
-  // }
   render() {
     return <div>
         <Login/>
-        {/* {this.state.user ? <Home /> : <Login />} */}
     </div>;
   }
 }
